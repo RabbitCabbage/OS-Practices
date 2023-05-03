b1ad3b53d9d0ab1d07620095068cb526bf48a268
shen-dong <shen-dong@sjtu.edu.cn>
2023-05-03 22:33:10 +0800

    Buddy memory management

--- practice-2/practice_2-1/buddy.c
+++ practice-2/practice_2-1/buddy.c
@@ -1,17 +1,81 @@
 #include "buddy.h"
+#include "tool.h"
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
 #define NULL ((void *)0)
+struct buddy_manager* b;
+void init_node(struct block *root, int order, void *start){
+    root->start = start;
+    root->order = order;
+    root ->next_in_freelist = NULL;
+    root ->prev_in_freelist = NULL;
+    root->status = FAKE;
+    if(order == 1)return;// no left or right child to init
+    root->left_child = (struct block *)malloc(sizeof(struct block));
+    root->right_child = (struct block *)malloc(sizeof(struct block));
+    root->right_child->parent = root;
+    root->left_child->parent = root;
+    root->left_child->buddy = root->right_child;
+    root->right_child->buddy = root->left_child;
+    init_node(root->left_child, order-1, start);
+    init_node(root->right_child, order-1, start+(int)pow(2,order-1)*4096);
+    return;
+}
 int init_page(void *p, int pgcount){
+    //初始化 buddy 页面。你需要管理pgcount个 4K 页，这些 4K 页连续分配并且起始地址是p
+    //阶数要可以从0到log(pgcount)+1，那么freelist的空间就应该是log(pgcount)+2
+    //一共是pgcount个4k页面，一个页面需要4*1024个byte，所以需要分配出来pgcount*4096个byte 
+    b = (struct buddy_manager *)malloc(sizeof(struct buddy_manager));
+    b->start = p;
+    b->pgcount = pgcount;
+    b->maxorder = (int)log2(pgcount)+1;
+    b->freelist = (struct free_head_node *)malloc(sizeof(struct free_head_node)*(b->maxorder+1));
+    for(int i = 0; i < b->maxorder+1; i++){
+        b->freelist[i].order = i;
+        b->freelist[i].next = NULL;
+        b->freelist[i].block_num = 0;
+    }
+    b->root_block = (struct block *)malloc(sizeof(struct block));
+    init_node(b->root_block, b->maxorder, p);
+    b->root_block->parent = NULL;
+    b->root_block->buddy = NULL;
+    b->root_block->status = FREE;
+    b->freelist[b->maxorder].next = b->root_block;
+    // todo maybe something wrong with pagerank
     return OK;
 }
 void *alloc_pages(int rank){
-    return NULL;
+    //分配rank阶的页面，如果没有足够的页面，返回NULL
+    int i;
+    for(i = rank;i<b->maxorder;++i){
+        if(b->freelist[i].next != NULL)break;
+    }
+    if(i>=b->maxorder){
+        printf("no enough pages\n");
+        return NULL;//没有足够的页面
+    }
+    //得到应该去rank=i里面拿节点
+    //首先把这个节点从freelist里面删掉
+    struct block* bl = pop_head_freelist(b,i);// 这个节点拿出来了并且把它变成了FAKE
+    //把这个节点分裂成rank阶的节点，期间总是把一半拿出去变成free
+    while(bl->order!=rank)bl = split_and_put_one_free(b,bl);
+    use_block(b,bl);
+    return bl->start;    
 }
 int return_pages(void *p){
+    //释放p指向的页面，如果p不是alloc_pages返回的地址，返回EINVAL
+    struct block *bl = find_block_in_buddy(b,p);
+    if(bl == NULL||bl->status == FREE) return -EINVAL;
+    //merge the blocks
+    return_and_merge(b,bl);
     return OK;
 }
 int query_ranks(void *p){
-    return OK;
+    struct block *bl = find_block_in_buddy(b,p);
+    if(bl == NULL) return -EINVAL;
+    else return bl->order;
 }
 int query_page_counts(int rank){
-    return OK;
+    return b->freelist[rank].block_num;
 }
---
+++ practice-2/practice_2-1/tool.h
@@ -0,0 +1,107 @@
+#ifndef TOOL_H
+#define TOOL_H
+#define NULL 0
+#define FREE 1
+#define USED 2
+#define FAKE 3
+
+//吊在每一阶free_list上的节点
+// 同时也是在地址树上面的节点
+struct block{
+    //起始地址
+    void *start;
+    //还有这个节点的阶数，表示的是这个节点中页的大小，可以用来计算结束地址
+    int order;
+    //还有一个指向下一个节点的指针
+    struct block *next_in_freelist;
+    struct block *prev_in_freelist;
+    struct block *left_child;
+    struct block *right_child;
+    struct block *parent;
+    struct block *buddy;
+    int status;//表示在树上的节点状态，是free还是used还是fake
+    //free表示的是在freelist里面并且可以分配，used表示exactly可以使用，fake表示的只是在树上当一个假的节点并不能用来分配或者归还
+};
+struct free_head_node{
+    int order;//表示这一个表头的阶数
+    int block_num;
+    struct block *next;//指向这个表头的第一个节点,一开始会把它初始化成NULL
+};
+struct buddy_manager{
+    struct free_head_node *freelist;//指向一个freelist的指针
+    void *start;//指向这个buddy管理的内存的起始地址
+    int pgcount;//表示这个buddy管理的内存的页数
+    int maxorder;//表示这个buddy管理的内存的最大阶数，也就是freelist的最大阶数，log(pgcount)+1
+    struct block *root_block;
+};
+
+void add_to_freelist(struct buddy_manager *b, struct block *block_to_add){
+    //首先要找到这个block的阶数
+    int order = block_to_add->order;
+    //然后把这个block加到这个阶数的freelist的最前面
+    block_to_add->next_in_freelist = b->freelist[order].next;
+    block_to_add->prev_in_freelist = NULL;
+    if(b->freelist[order].next != NULL){
+        b->freelist[order].next->prev_in_freelist = block_to_add;
+    }
+    b->freelist[order].next = block_to_add;
+    //然后应该把这个block的status改成free
+    block_to_add->status = FREE;
+    b->freelist[order].block_num++;
+    return;
+}
+struct block *pop_head_freelist(struct buddy_manager *b,int rank){
+    //首先要找到这个rank的freelist的头节点
+    struct block *head = b->freelist[rank].next;
+    //然后把这个头节点从freelist里面拿出来
+    if(head != NULL){
+        b->freelist[rank].next = head->next_in_freelist;
+        if(head->next_in_freelist != NULL){
+            head->next_in_freelist->prev_in_freelist = NULL;
+        }
+        head->next_in_freelist = NULL;
+        head->prev_in_freelist = NULL;
+        head->status = FAKE;
+    }
+    b->freelist[rank].block_num--;
+    return head;
+}
+
+struct block* split_and_put_one_free(struct buddy_manager *b, struct block* to_split){
+    to_split->status = FAKE;
+    // 然后把它的左孩子返回出去，右孩子记成free
+    add_to_freelist(b,to_split->right_child);
+    return to_split->left_child;
+}
+
+void use_block(struct buddy_manager *b, struct block *to_use){
+    to_use->status = USED;
+}
+
+struct block *find_block_on_tree(struct block *root, void *p){
+    if(root->start == p&&root->status != FAKE){
+        return root;
+    }else {
+        if(root->left_child==NULL)return NULL;
+        else if(p<root->right_child->start)return find_block_on_tree(root->left_child,p);
+        else return find_block_on_tree(root->right_child,p);
+    }
+}
+
+struct block *find_block_in_buddy(struct buddy_manager *b, void *p){
+    if((p-b->start)%4096!=0)return NULL;
+    if(((p-b->start)/4096)%2!=0)return NULL;
+    return find_block_on_tree(b->root_block,p);
+}
+
+void return_and_merge(struct buddy_manager *b, struct block * bl){
+    bl->status = FREE;
+    while(bl->buddy->status == FREE){
+        bl->status = FAKE;
+        bl->buddy->status = FAKE;
+        bl->parent->status = FREE;
+        bl = bl->parent;
+    }
+    return;
+}
+#endif
